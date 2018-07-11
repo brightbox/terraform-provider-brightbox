c4e574a715d1fbb9104808681a754500a7f3229f
Neil Wilson <neil@aldur.co.uk>
2018-07-11 15:22:32 +0100

    Add additional import tests for Brightbox Server

--- brightbox/import_brightbox_server_test.go
+++ brightbox/import_brightbox_server_test.go
@@ -28,3 +28,92 @@ func TestAccBrightboxServer_importBasic(t *testing.T) {
 		},
 	})
 }
+
+func TestAccBrightboxServer_importBlank(t *testing.T) {
+	resourceName := "brightbox_server.foobar"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckBrightboxServerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckBrightboxServerConfig_blank,
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccBrightboxServer_importUserDataBase64(t *testing.T) {
+	resourceName := "brightbox_server.foobar"
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckBrightboxServerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckBrightboxServerConfig_base64_userdata(rInt),
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{"user_data_base64"},
+
+			},
+		},
+	})
+}
+
+func TestAccBrightboxServer_importMultiServerGroup(t *testing.T) {
+	resourceName := "brightbox_server.foobar"
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckBrightboxServerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckBrightboxServerConfig_multi_server_group(rInt),
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccBrightboxServer_importServerGroup(t *testing.T) {
+	resourceName := "brightbox_server.foobar"
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckBrightboxServerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckBrightboxServerConfig_server_group(rInt),
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
