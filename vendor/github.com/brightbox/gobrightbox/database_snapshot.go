bcbe554989aeaf8b7485aecfb78e17c22ae7e3dc
Neil Wilson <neil@aldur.co.uk>
2019-03-20 14:03:02 +0000

    Update vendor

--- vendor/github.com/brightbox/gobrightbox/database_snapshot.go
+++ vendor/github.com/brightbox/gobrightbox/database_snapshot.go
@@ -20,3 +20,32 @@ type DatabaseSnapshot struct {
 	DeletedAt       *time.Time `json:"deleted_at"`
 	Locked          bool
 }
+
+// DatabaseSnapshots retrieves a list of all database snapshot
+func (c *Client) DatabaseSnapshots() ([]DatabaseSnapshot, error) {
+	var database_snapshot []DatabaseSnapshot
+	_, err := c.MakeApiRequest("GET", "/1.0/database_snapshots", nil, &database_snapshot)
+	if err != nil {
+		return nil, err
+	}
+	return database_snapshot, err
+}
+
+// DatabaseSnapshot retrieves a detailed view of one database snapshot
+func (c *Client) DatabaseSnapshot(identifier string) (*DatabaseSnapshot, error) {
+	database_snapshot := new(DatabaseSnapshot)
+	_, err := c.MakeApiRequest("GET", "/1.0/database_snapshots/"+identifier, nil, database_snapshot)
+	if err != nil {
+		return nil, err
+	}
+	return database_snapshot, err
+}
+
+// DestroyDatabaseSnapshot issues a request to destroy the database snapshot
+func (c *Client) DestroyDatabaseSnapshot(identifier string) error {
+	_, err := c.MakeApiRequest("DELETE", "/1.0/database_snapshots/"+identifier, nil, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
