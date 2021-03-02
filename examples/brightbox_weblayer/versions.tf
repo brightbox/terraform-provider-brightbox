c3ac366d7d81ffb5edcee3e72f1b8f434c0c8104
Neil Wilson <neil@aldur.co.uk>
2021-03-02 17:25:04 +0000

    Fix provider versions in examples

--- examples/brightbox_weblayer/versions.tf
+++ examples/brightbox_weblayer/versions.tf
@@ -2,8 +2,9 @@ terraform {
   required_providers {
     brightbox = {
       source  = "brightbox/brightbox"
-      version = "~> 1.5"
+      version = "~> 2.0"
     }
   }
+  required_version = ">= 0.12.26"
 }
 
